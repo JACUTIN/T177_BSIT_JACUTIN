c78917c5e6825393f79d865638baaef82e51c2ca
JACUTIN <115529649+JACUTIN@users.noreply.github.com>
2022-10-11 17:32:23 +0800

    Add files via upload

---
+++ Hello_world.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+int num1  = 2000;
+
+float num2  = 3.14;
+
+     printf ("Francois Andrei O. Jacutin\n I am 19 years old\n The value of num1 is %d\n and the value of num2 is %f", num1, num2);
+
+
+    return 0;
+
+
+}
+
+
+
+
